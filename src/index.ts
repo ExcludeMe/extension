e19815252ad07504b6009cc26c0178d96358840e
Leon™ <leontm.dev@gmail.com>
2023-08-11 16:39:02 +0200

    INIT

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+
+window.onload = () => {
+  let pornPack: string[] = ["porn", "xxx"];
+  let csgoPack: string[] = [
+    "csgo500",
+    "csgoroll",
+    "clash.gg",
+    "csgoluck",
+    "csgoempire",
+    "duelbits",
+    "gamdom",
+    "datdrop",
+    "hellcase",
+    "cs2dragon",
+    "wtfskins",
+    "csgolive",
+    "csgopolygon",
+    "rollbit",
+    "hypedrop",
+    "vloot",
+    "csgohowl",
+  ];
+  if ()
+};
