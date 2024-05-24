663b4067279211bcf7fd35eb910cbc2b502cb735
LeonTM <lanneken09@gmail.com>
2024-05-24 22:35:36 +0200

    Updated the script for the extension

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
-
 window.onload = () => {
-  let pornPack: string[] = ["porn", "xxx"];
-  let csgoPack: string[] = [
-    "csgo500",
-    "csgoroll",
-    "clash.gg",
-    "csgoluck",
-    "csgoempire",
-    "duelbits",
-    "gamdom",
-    "datdrop",
-    "hellcase",
-    "cs2dragon",
-    "wtfskins",
-    "csgolive",
-    "csgopolygon",
-    "rollbit",
-    "hypedrop",
-    "vloot",
-    "csgohowl",
-  ];
-  if ()
+  let exclude: boolean = false;
+  fetch("https://leontm.me/api/projects/excludeMe/collection/getAll", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.processable) {
+        let packs = data.responseData;
+        packs.forEach((pack: any) => {
+          let websites = pack.websites;
+          websites.forEach((website: any) => {
+            if (
+              window.location.origin.includes(website) ||
+              (website.includes(".") && window.location.href.includes(website))
+            ) {
+              exclude = true;
+            }
+          });
+        });
+      }
+    });
+  if (exclude) {
+    console.log("Redirecting to leontm.me");
+    window.location.href = `https://leontm.me/projects/excludeMe/excluded=${document.location.href}`;
+  }
 };
