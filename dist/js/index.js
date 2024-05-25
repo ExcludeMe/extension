9a2f8eb95d9c6bbd55b533f0cdfc44982b93c1b9
LeonTM <lanneken09@gmail.com>
2024-05-25 12:42:05 +0200

    Ran build

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,27 @@
 "use strict";
+window.onload = () => {
+    let exclude = false;
+    fetch("https://leontm.me/api/projects/excludeMe/collection/getAll", {
+        method: "GET",
+    })
+        .then((res) => res.json())
+        .then((data) => {
+        if (data.processable) {
+            let packs = data.responseData;
+            packs.forEach((pack) => {
+                let websites = pack.websites;
+                websites.forEach((website) => {
+                    if (window.location.origin.includes(website) ||
+                        (website.includes(".") && window.location.href.includes(website))) {
+                        exclude = true;
+                    }
+                });
+            });
+        }
+    });
+    if (exclude) {
+        console.log("Redirecting to leontm.me");
+        window.location.href = `https://leontm.me/projects/excludeMe/excluded=${document.location.href}`;
+    }
+};
 //# sourceMappingURL=index.js.map
